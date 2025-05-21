1f083dacd92554c889ce3e81ebcce4ff6d83e622
Vin Zhang <zhangzs000@hotmail.com>
2025-05-21 18:00:26 +0800

    feat: display space char

--- nvim/lua/plugins/indentLine.lua
+++ nvim/lua/plugins/indentLine.lua
@@ -2,10 +2,17 @@ return {
   "Yggdroot/indentLine",
   config = function()
     vim.g.indentLine_enabled = 1
+    vim.g.indentLine_fileTypeExclude = { "neo-tree" }
+    vim.g.indentLine_conceallevel = 2
+    vim.g.vim_json_conceal = 0
+    vim.g.markdown_syntax_conceal = 0
     vim.g.indentLine_char = '┊'
+    vim.g.indentLine_leadingSpaceEnabled = 1
+    vim.g.indentLine_leadingSpaceChar = ' '
     vim.opt.list = true
     vim.opt.listchars:append({
       tab = "¦ ",
+      space = "·",
       -- trail = "·",
       -- eol = "↴",
     })
